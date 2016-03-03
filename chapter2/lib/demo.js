10b8253060cb2038da62b064026a952dbbd8b3e7
zhongwei.lzw <zhongwei.lzw@alibaba-inc.com>
2016-03-03 18:49:07 +0800

    add init branch

---
+++ chapter2/lib/demo.js
@@ -0,0 +1,13 @@
+/*
+ * demo
+ * https://github.com//demo
+ *
+ * Copyright (c) 2016 
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+exports.awesome = function() {
+  return 'awesome';
+};

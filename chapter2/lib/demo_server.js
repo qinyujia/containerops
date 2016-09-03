d710409c0c9503412a00e0a81d00239e73b06639
qinyujia <756927432@qq.com>
2016-09-03 21:19:42 +0800

    Update demo_server.js

--- chapter2/lib/demo_server.js
+++ chapter2/lib/demo_server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! ' + text.awesome() + " again");
+  res.send('Hello World! ' + text.awesome() + " again and again");
 });
 
 var server = app.listen(3000, function () {
